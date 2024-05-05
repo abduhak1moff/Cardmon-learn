1e9367dc3a2f3013d87855900318b3d1b65845af
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-05-05 15:03:55 +0500

    +

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container text-center">Made by Azizbek</div>
+    </footer>
+  );
+};
+
+export default Footer;
